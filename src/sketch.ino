8b3678ffd517f9b0c2f5b451543ebe3f5b6039e0
Konrad Scorciapino <konr@konr.mobi>
2016-05-28 14:39:47 -0300

    Adding LED

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,15 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 7, 6, 5, 4);
 
-#define LED_PIN 13
 
-void setup()
-{
-    pinMode(LED_PIN, OUTPUT);
+// Max controllers
+float current_temperature = 50.0;
+float target_temperature = 70.0;
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("T=XX.XX  A=XX.XX");
+  lcd.setCursor(0, 1);
+  lcd.print("Aquecedor ligado");
 }
 
-void loop()
-{
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(900);
+void loop() {
+  lcd.setCursor(2, 0);
+  lcd.print(current_temperature);
+  lcd.setCursor(11, 0);
+  lcd.print(target_temperature);
+  delay(100);
 }
